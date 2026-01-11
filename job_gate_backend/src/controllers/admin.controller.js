b505968868203fb1139748664fa1d0299f960e00
Mohammed Zair <141706079+mohammed-zair@users.noreply.github.com>
2026-01-11 20:36:18 +0900

    made some changes

--- job_gate_backend/src/controllers/admin.controller.js
+++ job_gate_backend/src/controllers/admin.controller.js
@@ -352,7 +352,7 @@ exports.listAllCVs = async (req, res) => {
           attributes: ["user_id", "full_name", "email"],
         },
       ],
-      order: [["created_at", "DESC"]],
+      order: [["createdAt", "DESC"]],
       limit: 100,
     });
 
