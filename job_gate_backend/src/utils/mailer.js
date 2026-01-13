0f326c5b18ef42345601444f199a39b72a9a668e
Mohammed Zair <141706079+mohammed-zair@users.noreply.github.com>
2026-01-13 13:58:58 +0300

    made some changes

--- job_gate_backend/src/utils/mailer.js
+++ job_gate_backend/src/utils/mailer.js
@@ -1,14 +1,25 @@
 // file: src/utils/mailer.js
 const { Resend } = require("resend");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+function getResend() {
+  const key = process.env.RESEND_API_KEY;
+  if (!key) {
+    throw new Error("RESEND_API_KEY missing (check .env + restart with --update-env)");
+  }
+  return new Resend(key);
+}
 
 async function sendEmail({ to, subject, html, text }) {
+  const resend = getResend();
+
+  const fromName = process.env.SMTP_FROM_NAME || "Talents We Trust";
+  const fromEmail = process.env.SMTP_FROM_EMAIL || "onboarding@resend.dev";
+
   return resend.emails.send({
-    from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+    from: `${fromName} <${fromEmail}>`,
     to,
     subject,
-    html: html || `<pre>${text}</pre>`,
+    html: html || (text ? `<pre>${text}</pre>` : "<p></p>"),
   });
 }
 
